a59a62fd09095c9418e4b47cd0a88754f5013a5e
vitorsoaresferreira <vitorsoftwaredeveloper@gmail.com>
2026-06-05 21:27:00 -0300

    style: change style loading screen

--- src/screens/LoadingScreen/index.tsx
+++ src/screens/LoadingScreen/index.tsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { View, ActivityIndicator } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { styles } from "./styles";
+import { useStyles } from "./styles";
+import { useAppTheme } from "@/context/ThemeContext";
 import { LogoResgatar } from "@/components/Svg/Logo";
-import { COLORS } from "@/theme";
 
-export const LoadingScreen = () => {
+interface ILoadingProps {}
+export const LoadingScreen = ({}: ILoadingProps) => {
+  const styles = useStyles();
+  const { colors } = useAppTheme();
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <View style={styles.logoContainer}>
-          <LogoResgatar color={COLORS.muted} size={400} />
+          <LogoResgatar color={colors.primary} size={400} />
         </View>
 
         <ActivityIndicator
           size={300}
           animating={true}
-          color={COLORS.muted}
+          color={colors.primary}
           style={styles.loader}
         />
       </View>
--- src/screens/LoadingScreen/styles.ts
+++ src/screens/LoadingScreen/styles.ts
@@ -1,25 +1,34 @@
-import { COLORS, SPACING } from "@/theme";
+import { useAppTheme } from "@/context/ThemeContext";
+import { SPACING } from "@/theme";
+import { useMemo } from "react";
 import { StyleSheet } from "react-native";
 
-export const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: COLORS.background,
-  },
-  content: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    paddingHorizontal: SPACING.xl,
-  },
-  logoContainer: {
-    width: 160,
-    height: 160,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  loader: {
-    marginBottom: SPACING.md,
-    position: "absolute",
-  },
-});
+export function useStyles() {
+  const { colors } = useAppTheme();
+  return useMemo(
+    () =>
+      StyleSheet.create({
+        container: {
+          flex: 1,
+          backgroundColor: colors.background,
+        },
+        content: {
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          paddingHorizontal: SPACING.xl,
+        },
+        logoContainer: {
+          width: 160,
+          height: 160,
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        loader: {
+          marginBottom: SPACING.md,
+          position: "absolute",
+        },
+      }),
+    [colors],
+  );
+}
